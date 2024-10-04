2884ada9f2ac2bfe05bcb0a3a4bf9c8e11fb410f
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-10-03 22:23:19 -0300

    finalizado

---
+++ js/script.js
@@ -0,0 +1,25 @@
+
+function AddCarrosArray(){ 
+
+    let carros = [
+        {
+            "marca": "Fiat",
+            "modelo": "Uno com escada tunado",
+            "ano": 2024,
+            "cor": "preto"    
+        },
+        {
+            "marca": "Vw",
+            "modelo": "UP",
+            "ano": 2023,
+            "cor": "branco"
+        },
+        {
+            "marca": "Chevrolet",
+            "modelo": "Camaro",
+            "ano": 2022,
+            "cor": "Amarelo"
+        }
+    ];
+    console.log(carros);
+}
